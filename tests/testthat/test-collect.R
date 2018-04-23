97227b8ca492d8fcbdd3887747211b22b2061016
Victor Ordu <victorordu@outlook.com>
2018-04-23 09:53:23 +0100

    Fix failing test

--- tests/testthat/test-collect.R
+++ tests/testthat/test-collect.R
@@ -3,6 +3,5 @@
 context("Excel file collation")
 
 test_that("input is validated", {
-  expect_error(collate_all_excel(dest = "path/to/fake"),
-               "Directory ‘path/to/fake’ does not exist")
+  expect_error(collate_all_excel(dest = "path/to/fake"))
 })
