766457a2474531c259d5cc4128164cb0d076b6c2
Victor Ordu <victorordu@outlook.com>
2018-05-02 00:08:10 +0100

    Bump version
    Essentially added some verbosity to collection of Excel files. Also added
    more unit tests.

--- R/collect.R
+++ R/collect.R
@@ -7,15 +7,22 @@
 #'
 #' @param dir Root directory to commence search for files.
 #' @param dest Directory where collated files are stored.
+#' @param verbose Logical. Whether to display a printout of copy operations.
 #'
 #' @export
-collate_all_excel <- function(dir, dest = NULL)
+collate_excel <- function(dir, dest = NULL, verbose = TRUE)
 {
-  if (missing(dir)) dir <- "~/"
-  dir <- normalizePath(dir)
+  slsh <- "/"
+  if (missing(dir))
+    dir <- "~/"
+  dir <- normalizePath(dir, winslash = slsh)
 
+  ## Make sure we have a directory to keep the files.
   if (is.null(dest)) {
-    dest <- suppressWarnings(normalizePath(file.path(getwd(), "collate")))
+    dest <-
+      suppressWarnings(
+        normalizePath(
+          file.path(getwd(), "collated"), winslash = slsh))
     if (!dir.exists(dest))
       dir.create(dest)
   }
@@ -23,19 +30,54 @@ collate_all_excel <- function(dir, dest = NULL)
     stop(paste("Directory", sQuote(dest), "does not exist"))
   }
 
-  paths <- list.files(
-    dir,
-    pattern = ".xls$|.xlsx$",
-    recursive = TRUE,
-    all.files = TRUE,
-    ignore.case = TRUE,
-    full.names = TRUE
-  )
-  sapply(paths, function(x) {
-    try(file.copy(from = x, to = file.path(dest, basename(x))))
+  if (verbose)
+    cat("Root directory for this search is", sQuote(dir), ":\n\n")
+  cat("R is building the directory list. Please wait...\n")
+  dirLst <- normalizePath(list.dirs(dir), winslash = slsh, mustWork = TRUE)
+
+  ## We're getting the absolute path of Excel files that exist in the
+  ## directories found, and copying them to the final destination.
+  ## Note that '.lf()' is defined outside this scope.
+  paths <- lapply(dirLst, function(d) {
+    files <- .getThisDirExcelFiles(d, slsh)
+    if (length(files)) {
+      if (verbose) {
+        cat("Excel file(s) in", sQuote(basename(d)), ":\n* ")
+        cat(paste(basename(files), collapse = "\n* "), "\n")
+        cat("Copying files...\n")
+      }
+      success <- try(file.copy(from = files, to = dest))
+    }
   })
+  paths <- unlist(paths)
+  sp <- sum(paths)
   lp <- length(paths)
-  ld <- length(list.files(dest, all.files = TRUE))
-  if (!identical(lp, ld))
-    warning(sprintf("Only %d out of %d files found were collected", ld, lp))
+  if (verbose & (sp > 0)) {
+    cat("Files were successfully copied into",
+        sQuote(basename(dest)),
+        "\n")
+    if (!identical(lp, sp))
+      warning(sprintf("Only %d out of %d files found were collected", sp, lp))
+  }
+}
+
+
+
+
+
+
+## Customises base::list.files()
+.getThisDirExcelFiles <- function(d, s) {
+  stopifnot(identical(length(d), 1L))
+  normalizePath(
+    list.files(
+      path = d,
+      pattern = ".xls$|.xlsx$",
+      recursive = FALSE,
+      all.files = TRUE,
+      ignore.case = TRUE,
+      full.names = TRUE
+    ),
+    winslash = s
+  )
 }
