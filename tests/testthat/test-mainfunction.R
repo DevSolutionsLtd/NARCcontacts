19c74bcb31435444a2457cfdc5cc67fd029297fb
Victor Ordu <victorordu@outlook.com>
2018-05-06 16:37:23 +0100

    Add quiet option for harmonisation operation
    The harmonisation can now be done without any kind of output. Also the documentation was improved slightly.
    
    The function now selects only the first element of a character vector with length > 1.
    
    The usual warning for normalizePath() when non-existent paths are passed was suppressed in favour of a halting execution with an error message.

--- tests/testthat/test-mainfunction.R
+++ tests/testthat/test-mainfunction.R
@@ -9,9 +9,13 @@ test_that("the main function recognises illegal input", {
   expect_error(harmonise_narc_excel(9),
                "is.character(dir) is not TRUE",
                fixed = TRUE)
-  suppressWarnings(expect_error(
-    harmonise_narc_excel("fake"),
-    "There is no directory called ",
-    fixed = TRUE))
+  expect_error(harmonise_narc_excel("fake"),
+               "There is no directory called ",
+               fixed = TRUE)
+  expect_warning(harmonise_narc_excel(c("test-dir", "dud"), quiet = TRUE),
+                 "Length of 'dir' is > 1",
+                 fixed = TRUE)
   # expect_warning(harmonise_narc_excel("fake"), "The system cannot find ")
+
+  unlink("test-dir/harmonised-data", recursive = TRUE, force = TRUE)
 })
